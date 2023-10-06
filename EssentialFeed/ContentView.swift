dce495e1f2b2da48e339a5e32fad766ac047211c
Andreea Winandy <andreea.winandy@upstream-mobility.at>
2023-10-06 13:25:00 +0200

    Initial Commit

---
+++ EssentialFeed/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EssentialFeed
+//
+//  Created by Andreea Winandy on 06.10.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EssentialFeed/EssentialFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  EssentialFeedApp.swift
+//  EssentialFeed
+//
+//  Created by Andreea Winandy on 06.10.23.
+//
+
+import SwiftUI
+
+@main
+struct EssentialFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
